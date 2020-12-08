8c83abaca527feedc8c109d24d6f438b287ea378
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-08 17:53:24 +0100

    Read images as binary files

--- make_data_uris.py
+++ make_data_uris.py
@@ -5,25 +5,25 @@ import argparse
 
 
 def data_uri(sourcepath):
-  path = os.getcwd()
-  os.chdir(sourcepath)
-  data_uris = []
-  for filename in os.listdir('.'):
-    varname, filetype = os.path.splitext(filename)
-    mimetype = {
-        '.png': 'image/png',
-        '.svg': 'image/svg+xml'
-    }.get(filetype)
-    if not mimetype:
-        continue
-    filesize = os.stat(filename).st_size
-    if filesize > 10000:
-        continue
-    filecontent = open(filename).read()
-    b64 = base64.encodestring(filecontent).replace('\n', '')
-    data_uris.append('$data_uri_%(varname)s: "data:%(mimetype)s;base64,%(b64)s";' % locals())
-  os.chdir(path)
-  return data_uris
+    path = os.getcwd()
+    os.chdir(sourcepath)
+    data_uris = []
+    for filename in os.listdir('.'):
+        varname, filetype = os.path.splitext(filename)
+        mimetype = {
+            '.png': 'image/png',
+            '.svg': 'image/svg+xml'
+        }.get(filetype)
+        if not mimetype:
+            continue
+        filesize = os.stat(filename).st_size
+        if filesize > 10000:
+            continue
+        filecontent = open(filename, 'rb').read()
+        b64 = base64.encodebytes(filecontent).decode('ascii').replace('\n', '')
+        data_uris.append('$data_uri_%(varname)s: "data:%(mimetype)s;base64,%(b64)s";' % locals())
+    os.chdir(path)
+    return data_uris
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--source', action='append')
