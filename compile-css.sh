f73d5b2b83f922ef75964ed91e84b8ae7b9de86b
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-09 15:19:19 +0100

    Fix scss

--- compile-css.sh
+++ compile-css.sh
@@ -6,15 +6,16 @@ cd themes
 for theme in *
 do
   cd $BASE_DIR
-  echo "Building $theme..."
 
   static_dir=themes/$theme/static
 
+  echo "Making data URIs for $theme..."
 	python3 make_data_uris.py --source $static_dir/images/ \
     --source publik-base-theme/static/includes/img/ \
     --dest $static_dir/_data_uris.scss \
     --dest publik-base-theme/static/includes/_data_uris.scss
 
+  echo "Compiling CSS for $theme..."
   cd $static_dir && sassc style.scss style.css
 	rm -rf $static_dir/.sass-cache/
 done
