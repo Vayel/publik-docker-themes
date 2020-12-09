c9e515049bca2c38f1745c23daef0cb8e7b4e1d4
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-12-09 12:12:59 +0100

    Copy default style files

--- add-theme.py
+++ add-theme.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import json
+from shutil import copyfile
 
 
 def update_json(slug, label):
@@ -22,9 +23,16 @@ def create_theme(slug, label):
     theme_path = os.path.join("themes", slug)
     if os.path.isdir(theme_path):
         raise ValueError("Theme '{theme}' already exists.".format(theme=slug))
+
+    static_path = os.path.join(theme_path, "static")
     os.makedirs(theme_path)
-    os.makedirs(os.path.join(theme_path, "static"))
+    os.makedirs(static_path)
     os.makedirs(os.path.join(theme_path, "templates"))
+
+    open(os.path.join(static_path, "_custom.scss"), "w").close()
+    for fname in ("style.scss", "_vars.scss"):
+        copyfile(fname + ".template", os.path.join(static_path, fname))
+
     update_json(slug, label)
 
 
